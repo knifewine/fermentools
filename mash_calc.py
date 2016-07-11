3a00516ec976ae6937e8825075222e294b6265ff
Russ Hatch <russthehatch@gmail.com>
2016-07-10 20:10:04 -0600

    pep8

--- mash_calc.py
+++ mash_calc.py
@@ -4,9 +4,9 @@ from gi.repository import Gtk
 WATER_LB_PER_GAL_AT_150F = 8.18
 WATER_SPECIFIC_HEAT = 1
 GRAIN_ABSORPTION_GAL_PER_LB = 0.14
-GRAIN_SPECIFIC_HEAT = 0.38 # specific heat of grain, likely varies
-MY_MASH_TUN_WEIGHT = 9.13 # lb, that is
-MY_MASH_TUN_SPECIFIC_HEAT = 0.35 # specific heat of tun, varies depending on mash tun and materials
+GRAIN_SPECIFIC_HEAT = 0.38  # specific heat of grain, likely varies
+MY_MASH_TUN_WEIGHT = 9.13  # lb, that is
+MY_MASH_TUN_SPECIFIC_HEAT = 0.35  # specific heat of tun, varies depending on mash tun and materials
 
 
 def round_num(num):
@@ -14,6 +14,7 @@ def round_num(num):
     For consistent rounding behavior.
     """
     return round(num, 2)
+
 
 def calculate_water_absorbed(lbs_grain):
     """
@@ -81,11 +82,11 @@ class BatchSpageCalculatorWindow(Gtk.Window):
         Gtk.Window.__init__(self, title="Batch Sparge Calculator")
         self.set_border_width(15)
 
-        self.gal_into_boil  = gal_into_boil = Gtk.SpinButton(digits=2, numeric=True)
+        self.gal_into_boil = gal_into_boil = Gtk.SpinButton(digits=2, numeric=True)
         gal_into_boil.set_adjustment(Gtk.Adjustment(6, 0.25, 15, 0.25, 0.5, 0))
         gal_into_boil.connect("value-changed", self.recalculate)
 
-        self.grain_bill_lb  = grain_bill_lb = Gtk.SpinButton(digits=2, numeric=True)
+        self.grain_bill_lb = grain_bill_lb = Gtk.SpinButton(digits=2, numeric=True)
         grain_bill_lb.set_adjustment(Gtk.Adjustment(10, 0.25, 100, 0.25, 0.5, 0))
         grain_bill_lb.connect("value-changed", self.recalculate)
 
@@ -121,7 +122,7 @@ class BatchSpageCalculatorWindow(Gtk.Window):
         grid = Gtk.Grid(row_spacing=10)
         self.add(grid)
 
-        #inputs
+        # inputs
         grid.attach(Gtk.Label("Total Grain Bill (lbs)"), 0, 1, 1, 1)
         grid.attach(grain_bill_lb, 1, 1, 1, 1)
         grid.attach(Gtk.Label("Grist Ratio (qt/lb)"), 0, 2, 1, 1)
@@ -149,7 +150,6 @@ class BatchSpageCalculatorWindow(Gtk.Window):
         grid.attach(Gtk.Label("Sparge Water Temperature (F)"), 0, 12, 1, 1)
         grid.attach(calculated_sparge_temp, 1, 12, 1, 1)
 
-
     def recalculate(self, widget=None):
         grist_ratio = self.grist_ratio.get_value()
         dry_grain_weight = self.grain_bill_lb.get_value()
@@ -160,7 +160,7 @@ class BatchSpageCalculatorWindow(Gtk.Window):
         target_sparge_temp = self.target_sparge_temp.get_value()
 
         # do the math
-        strike_water_required = ( dry_grain_weight * grist_ratio ) / 4 # div by 4 to convert qt. to gal
+        strike_water_required = (dry_grain_weight * grist_ratio) / 4  # div by 4 to convert qt. to gal
         grain_absorption_gals = calculate_water_absorbed(dry_grain_weight)
         sparge_water_required = (gal_into_boil - strike_water_required) + grain_absorption_gals
 
